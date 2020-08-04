3a7f003f185ec9708f0fedeeaabd3b4ba9f7ece9
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-08-04 19:42:44 +0200

    end

--- firstSteps/main.js
+++ firstSteps/main.js
@@ -1,4 +1,14 @@
 
 //  hide inside scope
+// closure
 
 
+function showBalance(userName, amount) {
+  let total = amount;
+  function showUser() {
+    console.log(`${userName} have total ${total}$`);
+  }
+  return showUser();
+}
+
+showBalance('Ted', 400);
