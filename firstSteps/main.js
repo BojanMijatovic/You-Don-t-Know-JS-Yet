0702e28b31a6ef3c338ed0609fdbf99999eaa19e
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-09-14 20:24:26 +0200

    type coercion

--- firstSteps/main.js
+++ firstSteps/main.js
@@ -29,4 +29,45 @@ let newName = 'Bojan \"Mijatovic"';
 // console.log(newName);
 
 // template literals 
-console.log(`Half off 100 is ${100 / 2} ----- \"all numbers here are strings"`);  
+// console.log(`Half off 100 is ${100 / 2} ----- \"all numbers here are strings"`);   
+
+
+// ----- typeof operator-------
+
+// console.log(typeof 3);        //    number
+// console.log(typeof 'x');       //    string
+// console.log(typeof []);        //    object
+// console.log(typeof {});        //    object
+
+
+// console.log("Aardvark" < "Zoroaster"); // true
+// console.log('a' < 'A');  //false 
+// console.log('a' > 'Z'); // true
+// console.log('a' > 'B');  // true
+
+//  ------ There is only one value in JavaScript that is not equal to itself, and that is
+//NaN (“not a number”).
+// console.log(NaN == NaN) // false is not equal to itself
+
+// ------ Logical operators ------
+//  && and   || or  ! not 
+
+/*
+In practice, you can
+usually get by with knowing that of the operators we have seen so far,
+|| has
+the lowest precedence,
+then comes &&,
+then the comparison operators (>, ==,and so on), and then the rest.
+This order has been chosen such that, in typical
+expressions like the following one, as few parentheses as possible are necessary:
+*/
+
+// --------Automatic type conversion ------- // 
+
+console.log(typeof (5 - '1'));          //   number 4
+console.log(typeof ('5' + 1));          //   string 51
+console.log(typeof (1 + '5'));          //   string 15
+console.log(false == 0);                //    true
+console.log(null == undefined);    //    true
+console.log(undefined == 0);        //    false
