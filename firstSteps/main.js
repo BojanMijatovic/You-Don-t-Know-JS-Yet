ad793c310f5b4728252a76f22282cbd403160740
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-09-14 19:56:45 +0200

    start over again

--- firstSteps/main.js
+++ firstSteps/main.js
@@ -1,14 +1,32 @@
+const num = 14.4e4;  //e add decimals
+// console.log(num);
 
-//  hide inside scope
-// closure
+//const a = 2e2;
+// console.log(a);
+//const b = 1e2;
+// console.log(b);
+// console.log(a + b);
 
+//  ----          precedence of the operators * / + -
+const a = 44 + 3 * 2;
+//console.log(a); // 50
 
-function showBalance(userName, amount) {
-  let total = amount;
-  function showUser() {
-    console.log(`${userName} have total ${total}$`);
-  }
-  return showUser();
-}
+// -- % symbol is used to represent the remainder operation
 
-showBalance('Ted', 400);
+// --------- Strings ----------- //
+
+let name = 'Bojan';
+let lastName = 'Mijatovic';
+let fullName = `${name} ${lastName}`;
+// console.log(fullName);
+
+let sentence = 'Here is my first sentence \n and here is second sentence :) ';
+//console.log(sentence);
+
+let backSlash = 'New sentence \"with backslash" ';
+let newName = 'Bojan \"Mijatovic"';
+// console.log(backSlash);
+// console.log(newName);
+
+// template literals 
+console.log(`Half off 100 is ${100 / 2} ----- \"all numbers here are strings"`);  
