16d9693307bbafdf91fbb7951e2812b9f3c3ddbf
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-09-19 13:47:57 +0200

    Closure

--- firstSteps/jsFundamentals/functions.js
+++ firstSteps/jsFundamentals/functions.js
@@ -1,30 +1,87 @@
 
-const round = function (x) {
-  return x * x;
-}
+// const round = function (x) {
+//   return x * x;
+// }
 
 // console.log(round(4));
 
 
-const createRecipes = function (ingredient) {
-  const addPercentIngridient = function (amount) {
-    if (ingredient > 1) {
-      amount++;
-    }
-    console.log(`You have ${amount} ${ingredient}`);
-  }
-  addPercentIngridient(2);
-}
+// const createRecipes = function (ingredient) {
+//   const addPercentIngridient = function (amount) {
+//     if (ingredient > 1) {
+//       amount++;
+//     }
+//     console.log(`You have ${amount} ${ingredient}`);
+//   }
+//   addPercentIngridient(2);
+// }
 
 
 // createRecipes('tomato');
 // createRecipes('onion');
 
-const addNums = function (x) {
-  const addNew = function (y) {
-    return x + y;
-  }
-  return addNew(4);
+// const addNums = function (x) {
+//   const addNew = function (y) {
+//     return x + y;
+//   }
+//   return addNew(4);
+// }
+
+// console.log(addNums(10));
+
+const newX = function (x) {
+  let test = 1;       // local binding
+  return test + x;
 }
 
-console.log(addNums(10));
+// console.log(newX(4));
+
+
+//     ----  Function declaration ----
+function addNums(x, y) {
+  return x + y;
+}
+/*
+Function declarations are not part of the regular top-to-bottom
+flow of control. They are conceptually moved to the top of their scope and can
+be used by all the code in that scope.
+*/
+// console.log(addNums(4, 3));
+
+//   ------ Arrow Functions
+const name = (user) => {
+  return console.log(`Welcome ${user}`);
+}
+
+// name('Alex');
+
+//  add default parameter
+// const nums = (x, y = 4) => x + y;
+// console.log(nums(2, 10));
+
+
+// Closure 
+/*
+ In other words, a closure gives you access to an outer function’s scope from an inner function. In JavaScript, closures are created every time a function is created, at function creation time.
+*/
+
+const test = n => {
+  let a = n;
+  function returnN() {
+    return a;
+  }
+  return returnN();      //closure1
+}
+
+let b = test('word');
+console.log(b);
+
+
+const sumNums = (x) => {
+  function add(y) {
+    return x + y;
+  }
+  return add(5); // closure
+}
+const num = sumNums(4);
+console.log(num);
