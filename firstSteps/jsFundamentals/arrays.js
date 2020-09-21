9209108986b0f0eff78c757ae4110c5d5c89d109
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-09-21 21:28:53 +0200

    Arrays 3

--- firstSteps/jsFundamentals/arrays.js
+++ firstSteps/jsFundamentals/arrays.js
@@ -46,3 +46,21 @@ for (let i = 0; i < products.length; i++) {
   total += price;
   let itemText = name + ' — $' + price;*/
 }
+
+/*
+    Remove the last item in the array.
+    Add two new names to the end of the array.
+    Go over each item in the array and add its index number after the name inside parentheses, for example Ryu (0). Note that we don't teach how to do this in the Arrays article, so you'll have to do some research.
+    Finally, join the array items together in a single string called myString, with a separator of " - ".
+*/
+
+let myArray = ["Ryu", "Ken", "Chun-Li", "Cammy", "Guile", "Sakura", "Sagat", "Juri"];
+
+myArray.pop();
+myArray.push('Tesla', 'Alex');
+for (let i = 0; i < myArray.length; i++) {
+  let mySting = `${myArray[i]}-(${i})`;
+  // let addJoin = myArray.join('+');
+  // console.log(addJoin);
+  console.log(mySting);
+}
