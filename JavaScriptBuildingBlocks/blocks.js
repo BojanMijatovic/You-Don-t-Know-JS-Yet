418e8b2c529683b14fde3c44ed5a21518e931d90
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-09-22 21:44:18 +0200

    if else

--- JavaScriptBuildingBlocks/blocks.js
+++ JavaScriptBuildingBlocks/blocks.js
@@ -33,4 +33,27 @@ if (login == 'Employee') {
 }*/
 
 
-let message = (login == 'Employee') ? 'Hello' : (login == 'Director') ? 'Greetings' : (login == '') ? 'No login' : ' '; 
+// let message = (login == 'Employee') ? 'Hello' : (login == 'Director') ? 'Greetings' : (login == '') ? 'No login' : ' '; 
+
+// true || alert("not printed");
+// false || alert("printed");
+//In the first line, the OR || operator stops the evaluation immediately upon seeing true, so the alert isn’t run.
+
+let login = prompt('Who is there');
+
+if (login == 'admin') {
+  let password = prompt('enter password')
+
+  if (password == 'the master') {
+    console.log(`Welcome admin`);
+  } else if (password == '') {
+    console.log(`cancel`);
+  } else {
+    console.log(`Wrong password`);
+  }
+
+} else if (login == '') {
+  console.log('cancel');
+} else {
+  console.log(`I do not know you`);
+}
