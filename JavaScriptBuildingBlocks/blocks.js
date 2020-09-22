d1ea70a54c09a445f3480376bdf28a1bc19b67d4
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-09-22 20:52:40 +0200

    ternary operators

--- JavaScriptBuildingBlocks/blocks.js
+++ JavaScriptBuildingBlocks/blocks.js
@@ -19,3 +19,18 @@ In this task we assume that the input is always a number.
 //   console.log(`Num is zero`);
 // }
 
+/*
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}*/
+
+
+let message = (login == 'Employee') ? 'Hello' : (login == 'Director') ? 'Greetings' : (login == '') ? 'No login' : ' '; 
