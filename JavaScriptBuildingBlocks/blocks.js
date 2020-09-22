fe6895309d9d542ef44a2d350945381f1e687fab
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-09-22 20:46:27 +0200

    if else

---
+++ JavaScriptBuildingBlocks/blocks.js
@@ -0,0 +1,21 @@
+
+/*
+Using if..else, write the code which gets a number via prompt and then shows in alert:
+
+    1, if the value is greater than zero,
+    -1, if less than zero,
+    0, if equals zero.
+
+In this task we assume that the input is always a number.
+*/
+
+// let num = parseInt(prompt('enter num'));
+// console.log(num);
+// if (num > 1) {
+//   console.log(`Num ${num} is greater from zero`);
+// } else if (num < 0) {
+//   console.log(`Num ${num} is less then zero`);
+// } else {
+//   console.log(`Num is zero`);
+// }
+
