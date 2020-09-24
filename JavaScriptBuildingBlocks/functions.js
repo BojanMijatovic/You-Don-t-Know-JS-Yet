1a8b38ecfcbab273836dee1b5aaa44815267cd1b
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-09-24 19:09:28 +0200

    functions

--- JavaScriptBuildingBlocks/functions.js
+++ JavaScriptBuildingBlocks/functions.js
@@ -1,4 +1,4 @@
-function checkAge(age) {
+// function checkAge(age) {
   // if (age > 18) {
   //   return true;
   // } else {
@@ -8,9 +8,9 @@ function checkAge(age) {
   //   return console.log(age);
   // } return console.log('Did parents allow you ');
   // return (age > 18) ? console.log('You are welcome') : console.log('To young');
-}
-checkAge(2);
-checkAge(34);
+// }
+// checkAge(2);
+// checkAge(34);
 
 /*Write a function min(a,b) which returns the least of two numbers a and b.*/
 // function showMinNum(a, b) {
@@ -19,4 +19,62 @@ checkAge(34);
 // console.log(showMinNum(3, 11));
 // console.log(showMinNum(23, 11));
 
+
+// let x = 1;
+
+// function a() {
+//   let y = 2;
+//   output(y);
+// }
+
+// function b() {
+//   let z = 3;
+//   output(z);
+// }
+
+// function output(value) {
+//   const para = document.createElement('p');
+//   document.body.appendChild(para);
+//   para.textContent = 'Value: ' + value;
+// }
+
+// output(x);
+// a();
+// b();
+
+
+// function greatFunc() {
+//   let num = 1;
+
+//   smallFunc1(num);
+//   smallFunc2(num);
+//   smallFunc3(num);
+// }
+
+// function smallFunc1(a) {
+//   console.log(a);
+// }
+
+// function smallFunc2(a) {
+//   console.log(a);
+// }
+// function smallFunc3(a) {
+//   console.log(a);
+// }
+
+// greatFunc();
+
+// let names = ['Chris', 'Li Kang', 'Anne', 'Francesca', 'Mustafa', 'Tina', 'Bert', 'Jada']
+// let para = document.createElement('p');
+// // Add your code here
+// function chooseName(names) {
+//   let name = Math.floor(Math.random() * names.length);
+//   return names[name];
+// }
+
+// console.log(chooseName(names));
+
+// console.log(chooseName(names));
+
+// console.log(chooseName(names));
 
--- JavaScriptBuildingBlocks/loops.js
+++ JavaScriptBuildingBlocks/loops.js
@@ -23,23 +23,23 @@
 //   console.log(`${myArray[i]}`);
 // }
 
-let name = 'Tina';
+// let name = 'Tina';
 
-let phonebook = [
-  { name: 'Chris', number: '1549' },
-  { name: 'Li Kang', number: '9634' },
-  { name: 'Anne', number: '9065' },
-  { name: 'Francesca', number: '3001' },
-  { name: 'Mustafa', number: '6888' },
-  { name: 'Tina', number: '4312' },
-  { name: 'Bert', number: '7780' },
-  { name: 'Jada', number: '2282' },
-]
+// let phonebook = [
+//   { name: 'Chris', number: '1549' },
+//   { name: 'Li Kang', number: '9634' },
+//   { name: 'Anne', number: '9065' },
+//   { name: 'Francesca', number: '3001' },
+//   { name: 'Mustafa', number: '6888' },
+//   { name: 'Tina', number: '4312' },
+//   { name: 'Bert', number: '7780' },
+//   { name: 'Jada', number: '2282' },
+// ]
 
 
-for (let i = 0; i < phonebook.length; i++) {
-  let person = phonebook[i];
-  if (name == person.name) {
-    console.log(`Person ${person.name} and number is ${person.number}`);
-  }
-}
+// for (let i = 0; i < phonebook.length; i++) {
+//   let person = phonebook[i];
+//   if (name == person.name) {
+//     console.log(`Person ${person.name} and number is ${person.number}`);
+//   }
+// }
