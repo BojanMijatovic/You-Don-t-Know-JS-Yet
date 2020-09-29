95da4c3da2c97f1fee26e441a0ed8c000f458f0c
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-09-29 20:17:34 +0200

    Lodash

--- advancedConcept/objectsJS/functionalPrograming.js
+++ advancedConcept/objectsJS/functionalPrograming.js
@@ -2,7 +2,24 @@ const who = {
   name: 'Mrs White'
 }
 
-const suspect = who.name;
-console.log(suspect);
-who.name = 'Green'
-console.log(suspect);
+// let suspect = who.name;
+// console.log(suspect);
+// console.log(who);
+// who.name = 'Green';
+//  all property's are strings
+// console.log(typeof (who.name));   //  string :)
+
+const _ = {};
+
+if (_.name == undefined) {
+  _.name = 'new value';
+}
+console.log(_);
+
+_.set = function (obj, prop, value) {
+  return obj[prop] = value;
+}
+
+_.set(_, 'name', 'default');
+
+console.log(_);
