25e064de5cc962eb659eba650d8ae37756cd56c9
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-10-05 20:29:53 +0200

    obj

---
+++ advancedConcept/objectsJS/destructuring.js
@@ -0,0 +1,15 @@
+
+const amount = [100, 50];
+
+const [deposit, price] = amount;
+console.log(deposit * 2);
+
+
+const user = {
+  name: 'Alex',
+  lastName: 'Smith',
+  age: 37
+}
+
+const { name, lastName, age } = user;
+console.log(`${name} have ${age} years old`);
