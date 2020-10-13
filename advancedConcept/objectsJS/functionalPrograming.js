a682bd55ed99617321542d4f80827482a9232b8f
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-10-13 20:08:09 +0200

    Higher Order Functions

--- advancedConcept/objectsJS/functionalPrograming.js
+++ advancedConcept/objectsJS/functionalPrograming.js
@@ -1,1 +1,12 @@
 
+// const forEach = (array, fn) => {
+//   let i;
+//   for (i = 0; i < array.length; i++)
+//     fn(array[i])
+// }
+
+// export default forEach; 
+
+const arr = [1, 2, 3, 4];
+
+export default arr;
---
+++ advancedConcept/objectsJS/play.js
@@ -0,0 +1,4 @@
+
+import arr from './functionalPrograming.js';
+
+arr.forEach(item => console.log(`${item}`));
