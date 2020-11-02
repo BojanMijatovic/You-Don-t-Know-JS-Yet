a128d25b9b412d5e28996062470bfb96dbcba9f3
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-11-02 21:42:57 +0100

    add dom

--- startWithDom/script/main.js
+++ startWithDom/script/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const secretNum = Math.round(Math.random() * 20 + 1);
+
+console.log(secretNum);
+
+document.querySelector('.number').textContent = secretNum;
+
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guessNum = Number(document.querySelector('.guess').value);
+  console.log(guessNum);
+
+  if (!guessNum) {
+    document.querySelector('.message').textContent = 'Insert real value';
+  } else if (guessNum === secretNum) {
+    document.querySelector('.message').textContent = 'Correct Number'
+  }
+});
