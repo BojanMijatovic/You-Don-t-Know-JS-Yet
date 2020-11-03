7d5028615e4166a0a2442ea0a9776ac95f511fab
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-11-03 20:54:17 +0100

    add score gess

--- startWithDom/script/main.js
+++ startWithDom/script/main.js
@@ -1,12 +1,16 @@
 'use strict';
 
+//   create random num 
 const secretNum = Math.round(Math.random() * 20 + 1);
-
 console.log(secretNum);
+
+//  set score 
+let score = 20;
 
 document.querySelector('.number').textContent = secretNum;
 
 
+//  click on check btn 
 document.querySelector('.check').addEventListener('click', function () {
   const guessNum = Number(document.querySelector('.guess').value);
   console.log(guessNum);
@@ -15,5 +19,13 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Insert real value';
   } else if (guessNum === secretNum) {
     document.querySelector('.message').textContent = 'Correct Number'
+  } else if (guessNum > secretNum) {
+    document.querySelector('.message').textContent = 'To high';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guessNum < secretNum) {
+    document.querySelector('.message').textContent = 'To Low';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
