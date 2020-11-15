b1ddb48bb051fa2cee1186b84d12d81f48a49b69
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-11-15 17:57:43 +0100

    Destructuring Arrays

--- advancedConcept/objectsJS/destructuring.js
+++ advancedConcept/objectsJS/destructuring.js
@@ -5,5 +5,21 @@ const restaurant = {
   location: 'Bul... , Novi Sad , Serbia',
   categories: ['pizza', 'vegetarian', 'burgers'],
   mainMenu: ['pizza', 'pasta', 'breakfast'],
-  starterMenu: ['lunch', 'beer', 'juice']
+  starterMenu: ['lunch', 'beer', 'juice'],
+  orderFood(menuIndex, startMenuIndex) {
+    return [this.mainMenu[menuIndex], this.starterMenu[startMenuIndex]]
+  }
 }
+
+// const [pizza, , burgers] = restaurant.categories;
+
+
+
+let [first, second] = restaurant.categories;
+
+
+
+[first, second] = [second, first];
+
+const [order1, order2] = restaurant.orderFood(0, 0);
+console.log(order1, order2);
