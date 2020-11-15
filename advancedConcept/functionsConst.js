c3a80b8b89765b8ecfb73f27731f49266e438ba6
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-11-15 17:24:20 +0100

    add

--- advancedConcept/functionsConst.js
+++ advancedConcept/functionsConst.js
@@ -20,13 +20,13 @@ first();
 
 
 //   this word
-const person = {
-  name: 'Alex',
-  showAge: () => console.log(this),
-  show() {
-    console.log(this);
-  }
-}
+// const person = {
+//   name: 'Alex',
+//   showAge: () => console.log(this),
+//   show() {
+//     console.log(this);
+//   }
+// }
 
 // person.showAge();
 // person.show();
@@ -35,3 +35,42 @@ const matilda = {
   year: 2017,
   name: 'Test'
 }
+
+
+const a = {
+  name: 'A',
+  show() {
+    console.log(this);
+  },
+  newObj: {
+    newName: 'B',
+    showB() {
+      console.log(this);
+    },
+    showArr: () => {
+      console.log(this);
+    }
+  }
+}
+
+// a.show();                    // show A obj
+// a.newObj.showB();    // show newObj
+// a.newObj.showArr();  // show  window obj
+
+
+const person = {
+  name: 'Alex',
+  year: 20,
+
+  showAge: function () {
+
+    console.log(this.year);
+    let addAge = () => {
+      console.log(this.year + 20);
+    }
+
+    return addAge();
+  }
+}
+
+person.showAge();
