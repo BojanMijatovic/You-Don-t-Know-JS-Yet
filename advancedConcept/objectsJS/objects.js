4675ecf917acf2d1064d97548fb4efb6399f525a
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-11-21 20:34:45 +0100

    strings

--- advancedConcept/objectsJS/objects.js
+++ advancedConcept/objectsJS/objects.js
@@ -1,13 +1,28 @@
 'use strict';
 
+
+
 const userName = 'pEteR';
 
 const lowerName = userName.toLowerCase();
-const name = lowerName[0].toUpperCase().concat(lowerName.slice(1))
-console.log(name);
+// const name = lowerName[0].toUpperCase().concat(lowerName.slice(1))
+// console.log(name);
 
 
 const plane = 'Here is new framework with new js';
 
 const newStr = plane.replaceAll('new', 'test');
 console.log(newStr);
+
+
+const name = 'new sir alan ford';
+
+const capitalizeName = function (str) {
+  const newName = str.split(' ');
+  const test = [];
+  for (const n of newName) {
+    test.push(n[0].toUpperCase() + n.slice(1));
+  }
+  return test.join(' ');
+}
+console.log(capitalizeName(name));
