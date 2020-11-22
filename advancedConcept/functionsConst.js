bdc4ede28f2de90975a9e4f7e1fc8f37d68af536
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-11-22 19:25:13 +0100

    Callback Functions

--- advancedConcept/functionsConst.js
+++ advancedConcept/functionsConst.js
@@ -1,30 +1,13 @@
 'use strict';
 
-// const bookingArr = []
-
-
-// const createBooking = function (flightNumber, numberPassengers = 1, price = 150 * numberPassengers) {
-
-//   const booking = {
-//     flightNumber,
-//     numberPassengers,
-//     price
-//   }
-//   bookingArr.push(booking);
-//   console.log(booking);
-// }
-
-// createBooking('LH123');
-// createBooking('LH123', 3);
-
-const addNums = function (num1, num2) {
-  return `${num1 + num2}`;
-}
-
-const multiplyNums = function (multi, fu) {
-  return fu(2, 2) * multi;
+const addUser = function (name, lastName) {
+  return `${name} ${lastName}`;
 }
 
 
+const showUser = function () {
+  const name = addUser('Alex', 'Red');
+  return `Welcome ${name}`
+}
 
-console.log(multiplyNums(3, addNums));
+console.log(showUser());
