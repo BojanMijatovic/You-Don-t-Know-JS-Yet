ec46c56a8d6f2e417f151dc00bae3c207b005210
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-11-23 19:51:54 +0100

    The call and apply Methods

--- advancedConcept/functionsConst.js
+++ advancedConcept/functionsConst.js
@@ -1,13 +1,34 @@
 'use strict';
 
-const addUser = function (name, lastName) {
-  return `${name} ${lastName}`;
+// const greet = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+// greet('Hi')('Alex')
+
+
+const lufthansa = {
+  airline: 'lufthansa',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(`${name} booked seat on ${flightNum} on ${this.airline} flight `);
+    this.bookings.push({ flight: flightNum, company: this.airline, passengerName: name })
+  }
 }
 
+console.log(lufthansa);
 
-const showUser = function () {
-  const name = addUser('Alex', 'Red');
-  return `Welcome ${name}`
+lufthansa.book('lh213', 'Alex');
+lufthansa.book('lh4741531', 'Fred');
+
+const eurowings = {
+  airline: 'eurowings',
+  bookings: []
 }
 
-console.log(showUser());
+//  const book is now regular function, this word is now undefined! its not a methd now 
+const book = lufthansa.book;
+
+console.log(book);
+
+book.call(eurowings, '21312', 'Sara');
+
+console.log(eurowings);
