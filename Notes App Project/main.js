6e60df0c93975780a7409998e2b1200ba66333c1
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-12-06 11:19:41 +0100

    start new simple project app

---
+++ Notes App Project/main.js
@@ -0,0 +1,12 @@
+
+const title = document.querySelector('.title');
+
+//  search 
+const searchNotes = document.getElementById('search-notes');
+const searchBtn = document.getElementById('searchBtn');
+
+//  note
+const notes = document.querySelector('.notes');
+const note = document.querySelector('.note');
+const createNoteBtn = document.querySelector('.createNote');
+const removeAllNotesBtn = document.querySelector('.removeNotes');
