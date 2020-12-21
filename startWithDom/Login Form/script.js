093103d55cb5c1e8a0d54a2801a8a386fef0a434
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-12-21 21:59:43 +0100

    add

--- startWithDom/Login Form/script.js
+++ startWithDom/Login Form/script.js
@@ -1,7 +1,16 @@
 const loginForm = document.querySelector('.login');
 const form = document.getElementById('form');
 const messages = document.querySelector('.messages');
+const showBtn = document.querySelector('.show');
+const displayBtn = document.querySelector('.displayBtn');
 
+
+// show form
+showBtn.addEventListener('click', () => {
+  displayBtn.classList.add('login');
+  loginForm.classList.remove('login');
+  loginForm.classList.add('showLogin');
+})
 
 
 //  form name and pin
@@ -11,7 +20,8 @@ form.addEventListener('submit', function (e) {
   const pin = document.getElementById('pin');
 
   if (name.value == '' || Number(pin.value) == false) {
-    messages.innerHTML = `<div class="fail"> Insert real values </div>`;
+    messages.innerHTML = `<div class="fail"> Insert real values <br>
+    </div>`;
   } else {
     messages.innerHTML = `<div class="success"> Welcome: ${name.value} your pin is ${pin.value}</div>`;
     name.value = '';
