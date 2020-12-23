db3e76f78f4a62f742d915160a8ba22a85396b4a
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-12-23 20:01:15 +0100

    add

--- startWithDom/Login Form/script.js
+++ startWithDom/Login Form/script.js
@@ -1,32 +1,61 @@
-const loginForm = document.querySelector('.login');
-const form = document.getElementById('form');
-const messages = document.querySelector('.messages');
-const showBtn = document.querySelector('.show');
-const displayBtn = document.querySelector('.displayBtn');
+// const loginForm = document.querySelector('.login');
+// const form = document.getElementById('form');
+// const messages = document.querySelector('.messages');
+// const showBtn = document.querySelector('.show');
+// const displayBtn = document.querySelector('.displayBtn');
 
 
-// show form
-showBtn.addEventListener('click', () => {
-  displayBtn.classList.add('login');
-  loginForm.classList.remove('login');
-  loginForm.classList.add('showLogin');
-})
+// // show form
+// showBtn.addEventListener('click', () => {
+//   displayBtn.classList.add('login');
+//   loginForm.classList.remove('login');
+//   loginForm.classList.add('showLogin');
+// })
 
 
-//  form name and pin
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const name = document.getElementById('name');
-  const pin = document.getElementById('pin');
+// //  form name and pin
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+//   const name = document.getElementById('name');
+//   const pin = document.getElementById('pin');
 
-  if (name.value == '' || Number(pin.value) == false) {
-    messages.innerHTML = `<div class="fail"> Insert real values <br>
-    </div>`;
-  } else {
-    messages.innerHTML = `<div class="success"> Welcome: ${name.value} your pin is ${pin.value}</div>`;
-    name.value = '';
-    pin.value = '';
-  }
-})
+//   if (name.value == '' || Number(pin.value) == false) {
+//     messages.innerHTML = `<div class="fail"> Insert real values <br>
+//     </div>`;
+//   } else {
+//     messages.innerHTML = `<div class="success"> Welcome: ${name.value} your pin is ${pin.value}</div>`;
+//     name.value = '';
+//     pin.value = '';
+//   }
+// })
+// var x = document.getElementById("myLinks");
 
+
+mobiscroll.nav('#demo-icon', {
+  theme: 'ios',
+  themeVariant: 'dark',
+  type: 'hamburger'
+});
+
+mobiscroll.nav('#demo-icon-text', {
+  theme: 'ios',
+  themeVariant: 'dark',
+  type: 'hamburger',
+  menuText: 'Menu'
+});
+
+mobiscroll.nav('#demo-text', {
+  theme: 'ios',
+  themeVariant: 'dark',
+  type: 'hamburger',
+  menuIcon: '',
+  menuText: 'Menu'
+});
+
+mobiscroll.nav('#demo-gear', {
+  theme: 'ios',
+  themeVariant: 'dark',
+  type: 'hamburger',
+  menuIcon: 'line-settings'
+});
 
