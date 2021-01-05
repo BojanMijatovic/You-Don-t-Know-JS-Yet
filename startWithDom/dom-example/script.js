2eb82679ce931c4d4e7b15c33e0b8dfbacece039
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-01-05 12:17:44 +0100

    add js

--- startWithDom/dom-example/script.js
+++ startWithDom/dom-example/script.js
@@ -1,4 +1,3 @@
-
 const allLinks = document.querySelectorAll('.__items');
 const btn = document.querySelector('.add-item');
 const remove = document.querySelector('.remove');
@@ -7,17 +6,17 @@ console.log(allLinks);
 
 
 btn.addEventListener('click', () => {
-  const item = document.createElement('div');
-  item.classList.add('new');
-  item.textContent = 'Here is new item';
-  document.body.append(item);
+    const item = document.createElement('div');
+    item.classList.add('new');
+    item.textContent = 'Here is new item';
+    document.body.append(item);
 })
 
 //  toggle nav
 remove.addEventListener('click', () => {
-  allLinks.forEach(i => {
-    i.classList.toggle('display') ? remove.textContent = 'add nav' : remove.textContent = 'remove nav';
-  })
+    allLinks.forEach(i => {
+        i.classList.toggle('display') ? remove.textContent = 'add nav' : remove.textContent = 'remove nav';
+    })
 })
 
 
@@ -27,3 +26,30 @@ remove.addEventListener('click', () => {
 //     item.remove();
 //   })
 // })
+
+
+
+//  one more way to remove single item from arr
+const tools = document.querySelectorAll('.tool');
+tools.forEach(tool => {
+    const a = document.createElement('p');
+    a.textContent = 'text';
+    a.classList.add('hero');
+    tool.append(a);
+    a.addEventListener('click', function() {
+        tool.remove();
+    })
+})
+
+
+
+//  scroll to sections
+const scrollBtn = document.querySelector('.scroll');
+
+// scrollBtn.scrollIntoView({ behavior: 'smooth' });
+// scrollBtn.addEventListener('click', window.scrollIntoView({ behavior: 'smooth' }));
+
+scrollBtn.addEventListener('click', () => {
+    const section = document.getElementById('one');
+    section.scrollIntoView({ behavior: 'smooth' });
+})
