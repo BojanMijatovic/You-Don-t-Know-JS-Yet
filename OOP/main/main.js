7779ea77a8aa9056e3878f301390fd81d2d10819
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-01-27 21:39:48 +0100

    new classes

--- OOP/main/main.js
+++ OOP/main/main.js
@@ -34,7 +34,7 @@ class Bmw extends Car {
         this.gearBox = gearBox;
         this.doors = doors;
     }
-    showInfoModel() {
+    getInfoModel() {
         return console.log(`${this.model} type ${this.type},max speed is ${this.speed} km/h with gear box ${this.gearBox} levels`);
     }
 }
@@ -42,5 +42,24 @@ class Bmw extends Car {
 
 const zx3 = new Bmw('bmw', 'zx3', 200, 5, 5);
 
-zx3.showInfoModel();
+zx3.getInfoModel();
 zx3.accelerate(20);
+
+
+class ElX5 extends Bmw {
+    constructor(model, type, speed, gearBox, doors, battery, diskBreaks) {
+        super(model, type, speed, gearBox, doors);
+        this.battery = battery;
+        this.diskBreaks = diskBreaks;
+    }
+
+    accelerate(val) {
+        this.speed += val;
+        this.battery--;
+        return console.log(`On accelerate ${val}km/h your battery is ${this.battery}% speed now is ${this.speed}km/h`);
+    }
+}
+
+const x5 = new ElX5('bmw', 'zx3', 100, 6, 5, 100, 4);
+x5.accelerate(30);
+console.log(x5);
