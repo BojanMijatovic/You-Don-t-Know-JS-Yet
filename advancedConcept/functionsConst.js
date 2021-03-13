1763ba7695b715db4551ca40a9a9e79d36fa2634
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-03-13 12:24:29 +0100

    change

--- advancedConcept/functionsConst.js
+++ advancedConcept/functionsConst.js
@@ -1,18 +1,22 @@
+const showDay = function() {
+    const date = new Date();
+    const day = date.getDate(); // const a = date.toLocaleDateString(); or same with Locale Date String
+    const showDay = document.createElement('p');
+    // month 
+    const month = date.getMonth();
+    // year
+    const year = date.getFullYear();
+    // time
+    const time = date.toLocaleTimeString();
 
-
-const showDay = function () {
-  const date = new Date();
-  const day = date.getDate();      // const a = date.toLocaleDateString(); or same with Locale Date String
-  const showDay = document.createElement('p');
-  // month 
-  const month = date.getMonth();
-  // year
-  const year = date.getFullYear();
-  // time
-  const time = date.toLocaleTimeString();
-
-  showDay.textContent = `Today is ${day}/${month}/${year} and time is ${time} `;
-  document.body.append(showDay);
+    showDay.textContent = `Today is ${day}/${month}/${year} and time is ${time} `;
+    document.body.append(showDay);
 }
 
-showDay();
+// showDay();
+
+
+const str = 'Hello world';
+console.log(str.length);
+
+tese
