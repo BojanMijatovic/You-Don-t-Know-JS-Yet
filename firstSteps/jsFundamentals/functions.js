bae14bef844b395f26b00f566bf141bcd7cd9823
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-03-14 20:22:05 +0100

    arrow functions

--- firstSteps/jsFundamentals/functions.js
+++ firstSteps/jsFundamentals/functions.js
@@ -8,3 +8,30 @@ function showFruit(fruit, number) {
 //                                  here is arguments
 console.log(showFruit('apple', 5));
 console.log(showFruit('banana', 1));
+
+
+function calcComments(comments) {
+    return `We have ${comments} comments`
+}
+
+console.log(calcComments(4));
+
+function fullName(firstName, lastName) {
+    return `Hello my name is ${firstName} ${lastName}`
+}
+
+const names = fullName('Ted', 'Jonson');
+console.log(names);
+console.log(fullName('Alex', 'Red'));
+
+
+
+//  function expression
+const addSum = function(num1, num2) {
+    return num1 + num2;
+}
+
+const total = addSum(4, 19);
+console.log(total);
+
+//  Arrow functions
