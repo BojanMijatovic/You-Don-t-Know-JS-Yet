938a58b2a90c93d19e2ef13277e1e605d0015a48
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-03-15 23:50:03 +0100

    arrays

--- firstSteps/jsFundamentals/arrays.js
+++ firstSteps/jsFundamentals/arrays.js
@@ -8,5 +8,26 @@ const friends = ['MIchel', 'Alex', 'Ted'];
 
 const job = 'junior front-end developer'
 const jonas = ['Jonas', 'Tinder', job, 2021 - 1987, friends];
+// console.log(jonas);
 
-console.log(jonas);
+
+//  add element on end of array
+friends.push('Freddy');
+
+//  add element on start of array
+friends.unshift('Rachel');
+
+//  remove last element of array
+const lastFriend = friends.pop();
+console.log(lastFriend);
+
+// remove first element on array
+const firstFriend = friends.shift();
+
+//  find index of element in array
+const elementIndex = friends.indexOf('Ted');
+// console.log(elementIndex);
+
+//  includes method return boolean value is element in array
+const isElement = friends.includes('Tom');
+console.log(isElement);
