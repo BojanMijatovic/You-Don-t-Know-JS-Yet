f6a5e816953d943974ffff03d1a211ec9070b19c
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-03-18 21:29:38 +0100

    for loops

--- firstSteps/jsFundamentals/loops.js
+++ firstSteps/jsFundamentals/loops.js
@@ -1,59 +1,17 @@
-// ----- for loops
+'use strict';
 
-// for (let i = 0; i < 10; i++) {
-//   if (i % 2 == 0) {
-//     console.log(`I is ${i}`);
-//   }
-// }  
-
-// let num = 1;
-// for (i = 0; i < 10; i++) {
-//   num = num * 2;
-//   console.log(`Num is ${num}`);
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Let test this ${[i]}`);
 // }
-switch (key) {
-  case value:
 
-    break;
+const newArr = ['name', 'lastName', 2021 - 1987, [1, 2, 3, 4, 5]];
 
-  default:
-    break;
+
+const types = [];
+
+for (let i = 0; i < newArr.length; i++) {
+    console.log(`${newArr[i]}`);
+    types.push(`${typeof newArr[i]}`)
 }
 
-/*
-Zadatak 5
-
-Napisati for petlju koja prolazi kroz zadati niz i ispisuje elemente niza
-var niz = ["saab", "volvo", "mercedes"];
-
-
-Zadatak 6
-
-Napisati for petlju koja prolazi kroz zadati niz i ispisuje ga
-ali prekida sa izvrsavanjem ukoliko naidje na element "volvo"
-var niz = ["saab", "volvo", "mercedes"];
-
-
-Napisati for petlju koja prolazi kroz zadati niz i ispisuje ga
-ali preskace se ispisivanje ukoliko naidje na element "volvo"
-*/
-
-// let arr = ["saab", "volvo", "mercedes"];
-// for (i = 0; i < arr.length; i++) {
-//   if (arr[i] = 'volvo') {
-//     console.log(arr[i]);
-//     break;
-//   }
-// }
-
-// for (i = 0; i < arr.length; i++) {
-//   if (arr[i] == 'volvo') {
-//     console.log(`car is ${arr[i]}`);
-//     break;
-//   }
-// }
-
-// let km = prompt(`Enter the  km to convert to  miles`);
-// let miles = km * 1.60934;
-// console.log(`You have ${km * 1.6} miles`);
-
+console.log(types);
