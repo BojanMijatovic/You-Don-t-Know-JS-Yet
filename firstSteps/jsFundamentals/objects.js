a220ea690ae96a3c314ccb8532aadf2f78cd78a5
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-03-18 20:20:42 +0100

    obj

--- firstSteps/jsFundamentals/objects.js
+++ firstSteps/jsFundamentals/objects.js
@@ -1,10 +1,23 @@
 const ted = {
     name: 'ted',
     lastName: 'Alfonso',
-    age: 2021 - 1987,
-    friends: ['Tom', 'Alex', 'Peter']
+    birthYear: 1987,
+    friends: ['Tom', 'Alex', 'Peter'],
+    calcAge: function() {
+        return 2021 - this.birthYear;
+    }
 }
 
 ted['hobby'] = 'coding';
-
+ted['army'] = true;
 console.log(ted);
+
+// console.log(typeof ted['job']); 
+//                              undefined
+console.log(`${ted.name} has ${ted.friends.length} friends and his best friend is ${ted.friends[0]}`);
+
+
+
+/*
+    this keyword is point on object who is calling the method
+*/
