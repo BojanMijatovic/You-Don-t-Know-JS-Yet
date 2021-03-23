76b48df7b509b29510ebd21f9485c921e43b81a2
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-03-24 00:05:09 +0100

    add

--- startWithDom/template for practice/script.js
+++ startWithDom/template for practice/script.js
@@ -1,1 +1,5 @@
 const correctNumber = document.querySelector('.message').textContent = 'correct number';
+
+
+const number = document.querySelector('.number');
+const score = document.querySelector('.score');
